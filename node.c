36e8a2896f7a607529da372f8ed9d13833e01a82
Juan Barros <juan.barros@ccc.ufcg.edu.br>
2019-04-07 19:11:38 -0300

    Implementa PriorityQueue em C

---
+++ node.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct node {
+    int data;
+    int priority;
+    struct node* next;
+} Node;
+
+// Function to Create A New Node
+Node* newNode(int d, int p)
+{
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = d;
+    temp->priority = p;
+    temp->next = NULL;
+
+    return temp;
+}
+
+// Removes the element with the
+// highest priority form the list
+void pop(Node** head)
+{
+    Node* temp = *head;
+    (*head) = (*head)->next;
+    free(temp);
+}
+
+
+// Function to push according to priority
+void push(Node** head, int d, int p){
+    if((*head) == NULL){
+        Node* temp = newNode(d, p);
+        (*head) = temp;
+    } else if ((*head)->priority > p ) {
+        Node* temp = newNode(d, p);
+        temp->next = (*head);
+        (*head) = temp;
+    } else {
+        push(&((*head)->next), d+1, p);
+    }
+}
+
+// Function to check is list is empty
+int isEmpty(Node** head)
+{
+    return head == NULL || (*head) == NULL;
+}
